6c7d310a829dfb369b2877ac212d576a636b978c
moralea2 <moralea2@miamioh.edu>
2015-02-17 17:58:18 -0500

    Update Complex.java
    Added in comments

--- Complex.java
+++ Complex.java
@@ -1,31 +1,31 @@
 public class Complex {
 
 	float realNum, complexNum;
-
+	//The default constructor
 	public Complex() {
 		realNum = 0;
 		complexNum = 0;
 	}
-
+	//Constructor with custom values
 	public Complex(float realNum, float complexNum) {
 		this.realNum = realNum;
 		this.complexNum = complexNum;
 	}
-
+	//Simple complex number addition
 	public Complex add(Complex number) {
 		Complex c = new Complex();
 		c.realNum = this.realNum + number.realNum;
 		c.complexNum = this.complexNum + number.complexNum;
 		return c;
 	}
-
+	//Simple complex number subtraction
 	public Complex subtract(Complex number) {
 		Complex c = new Complex();
 		c.realNum = this.realNum - number.realNum;
 		c.complexNum = this.complexNum - number.complexNum;
 		return c;
 	}
-
+	//Simple complex number multiplication
 	public Complex multiply(Complex number) {
 		Complex c = new Complex();
 		c.realNum = (this.realNum * number.realNum)
@@ -34,7 +34,7 @@ public class Complex {
 				+ (this.realNum * number.complexNum);
 		return c;
 	}
-
+	//Simple complex number division
 	public Complex divide(Complex number) {
 		Complex c = new Complex();
 		c.realNum = (float) (((this.realNum * number.realNum) + (this.complexNum * number.complexNum)) / (Math
@@ -43,7 +43,7 @@ public class Complex {
 				.pow(number.realNum, 2) + Math.pow(number.complexNum, 2)));
 		return c;
 	}
-
+	//The toString method, returns the complex number as a string. 
 	@Override
 	public String toString() {
 		if(complexNum>0)
